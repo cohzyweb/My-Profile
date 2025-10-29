525bb47228677d52a89eaee8d801126a0874b2f7
Ogunjana Boluwatife <boluwatifeogunjana@gmail.com>
2025-10-29 18:02:03 +0100

    updated

--- src/components/ThemeToogle.jsx
+++ src/components/ThemeToogle.jsx
@@ -1,5 +1,6 @@
 import { Sun, Moon } from "lucide-react";
 import { useState, useEffect } from "react";
+import {cn} from "@/lib/utilis";
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
@@ -25,7 +26,11 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme} 
+    className= {cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+                   "focus:outline-hidden"
+    )}
+    >
       {isDarkMode ? 
         <Sun className="h-6 w-6 text-yellow-300" /> : 
         <Moon className="h-6 w-6 text-blue-900" />
