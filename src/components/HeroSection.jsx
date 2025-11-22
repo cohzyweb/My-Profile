ae8a3e320c1fda86672a9ac7391a66275b510c6a
Ogunjana Boluwatife <boluwatifeogunjana@gmail.com>
2025-11-22 21:21:06 +0100

    UPDATE

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,15 +1,26 @@
-
-
 export const HeroSection = () => {
     return <section id="hero" 
     className="relative min-h-screen flex flex-col items-center justify-center px-4">
         <div className="container max-w-4xl mx-auto text-center z-10">
-            <div className="spave-y-6">
-                <h1 className="text-4xl">
-                    <span>Hi, I'm </span>
-                    <span>Ogunjana </span>
-                    <span>Boluwatife</span>
+            <div className="space-y-6">
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                    <span className="opacity-0 animate-fade-in">Hi, I'm </span>
+                    <span className="text-primary opacity-0 animate-fade-in-delay-1">
+                        Ogunjana
+                    </span>
+                    <span className="text-gradient-primary ml-2 opacity-0 animate-fade-in-delay-2">Boluwatife</span>
                 </h1>
+
+                <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    I am a web developer who enjoys building clean, functional, and user-focused projects.
+                     I'm here to connect, learn from others, share what I know, and grow with the community. 
+                     Looking forward to collaborating, contributing, and meeting like-minded creatives here!
+                </p>
+                <div>
+                    <a href="#projects" className="cosmic-button">
+                        View My Work
+                    </a>
+                </div>
             </div>
         </div>
     </section>
