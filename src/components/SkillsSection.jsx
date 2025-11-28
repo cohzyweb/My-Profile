33045eaaee37512e1fca4bbe10b2ce7d9593590b
Ogunjana Boluwatife <boluwatifeogunjana@gmail.com>
2025-11-28 19:36:26 +0100

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -31,7 +31,11 @@ export const SkillsSection = () => {
                                 <h3 className="font-semibold text-lg">{skill.name}</h3>
                             </div>
                             <div className="w-full bg-secondary/50 h-2 roundd-full overflow-hidden">
-                            <div className="bg-primary h-2 rounded-full origin-left animate-" />
+                            <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out"
+                            style={{width: skill.level + "%"}} />
+                            </div>
+                            <div>
+                                <span>{skill.level}%</span>
                             </div>
                         </div>
                     ))}
